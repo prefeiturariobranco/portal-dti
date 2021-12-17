0d101a4dc67b252a7e26ba0e9217faed688760a5
jose gabriel <gabriel2007ff@hotmail.com>
2021-12-17 17:07:35 -0500

    att

--- resources/views/painel/cargos/cadastro.blade.php
+++ resources/views/painel/cargos/cadastro.blade.php
@@ -1,17 +1,19 @@
 @extends('templates.dashboard')
 @section('titulos')Cadastrar Cargos @endsection
 @section('content')
+    <div class="py-4"></div>
     <div class="container">
         <div class="card-header">
             <h4>Cadastrar Cargos</h4>
-
         </div>
-        <form class="row-cols-3">
-            <label class="py-4 col-form-label">Digite nome do cargo:</label>
-        <input class="form-control col-4" type="text" maxlength="255">
-            <br>
-        <input class="btn btn-outline-primary" type="submit" value="Cadastrar">
-            <button class="btn btn btn-outline-danger">Voltar</button>
-        </form>
+        <div class="card">
+            <form class="ml-4 row-cols-3" action="" method="POST">
+                <label class="py-4 col-form-label">Digite o nome do cargo:</label>
+                <input class="form-control col-4" type="text" maxlength="255">
+                <br>
+                <input class="btn btn-outline-primary" type="submit" value="Cadastrar">
+               <a class="btn btn btn-outline-danger" href="{{route('lista.cargos')}}">Voltar</a>
+            </form>
+        </div>
     </div>
 @endsection
