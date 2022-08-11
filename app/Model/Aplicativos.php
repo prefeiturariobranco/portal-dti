93c83a65b7feca079a54678927b069901beb15c2
jade <jadethomaz83@gmail.com>
2022-08-11 13:18:46 -0500

    mapas, auditoria

--- app/Model/Aplicativos.php
+++ app/Model/Aplicativos.php
@@ -3,12 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use OwenIt\Auditing\Auditable as AuditableTrait;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class Aplicativos extends Model implements Auditable
 {
-    use AuditableTrait;
+    use \OwenIt\Auditing\Auditable;
 
     protected $table = 'aplicativos';
     protected $fillable = [
